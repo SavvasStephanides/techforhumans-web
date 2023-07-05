95c6a321d30e187b9a6bf8449fd1e316aac9895f
root <savvascyp@hotmail.com>
2023-07-05 17:28:54 +0000

    Load post data

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,30 @@
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import axios from "axios"
+import { Link } from "@remix-run/react"
+
+export const loader = async () => {
+  const apiAllPostsUrl = "http://api/posts/all.json"
+  const apiResponse = await axios.get(apiAllPostsUrl)
+  const allPosts = apiResponse.data
+  
+  return json({
+    allPosts
+  })
+}
+
 export default function IndexRoute() {
-    return <div>Hello!</div>;
-  }
+    const data = useLoaderData<typeof loader>()
+    return <div>
+        <h1>All posts</h1>
+
+        <ul>
+         {
+          data.allPosts.map((post) => (
+            <li><Link to={`/explainer/${post.slug}`}>{post.title}</Link></li>
+          ))
+         }
+        </ul>
+      </div>;
+}
   
